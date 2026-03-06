f9249bf3a74a502949638609b185b09a22fe5ac4
Warren Scott <59210591+gogeekery@users.noreply.github.com>
2026-03-06 12:34:01 -0500

    Update avr_main.c

--- avr_main.c
+++ avr_main.c
@@ -23,17 +23,17 @@
 
 
 typedef struct {
-    uint16_t count;
-    uint16_t add;
-    uint16_t env_count;
+	uint16_t count;
+	uint16_t add;
+	uint16_t env_count;
 } Channel;
 
 typedef struct {
-    volatile uint8_t *port;
-    volatile uint8_t *pin;
-    uint8_t bit;
-    uint16_t note;
-    int8_t count;
+	volatile uint8_t *port;
+	volatile uint8_t *pin;
+	uint8_t bit;
+	uint16_t note;
+	int8_t count;
 } Key;
 
 
@@ -41,7 +41,7 @@ Channel channels[NUM_CHANNELS];
 
 const uint8_t (*songs[])[][3] = {
 
-    &spiritedaway_itsumo//,
+	&spiritedaway_itsumo//,
 
 };
 
@@ -63,102 +63,101 @@ uint8_t led_pwm_count = 0;
 uint8_t led_intensity = 0;
 
 int main(void) {
-    // power stuff
-    power_adc_disable();
-    power_spi_disable();
-    power_twi_disable();
-    power_usart0_disable();
-    power_timer2_disable();
 
-    // set outputs
-    set_output(SPKR1);
-    set_output(SPKR2);
+	power_adc_disable();
+	power_spi_disable();
+	power_twi_disable();
+	power_usart0_disable();
+	power_timer2_disable();
 
-    // initialize channel variables
-    for (int i = 0; i < NUM_CHANNELS; i++) {
-        channels[i].count = 0;
-        channels[i].add = 0;
-        channels[i].env_count = 0;
-    }
+	// --- Outputs ---
+	set_output(SPKR1);
+	set_output(SPKR2);
 
-    note_index = -1;
-    delay = 1;
+	for (int i = 0; i < NUM_CHANNELS; i++) {
+		channels[i].count = 0;
+		channels[i].add = 0;
+		channels[i].env_count = 0;
+	}
 
-    // Timer 0 for PWM of speaker pins (to simulate amplitudes)
-    // Fast PWM, 16 MHz tick
-    TCCR0A |= _BV(COM0A1) | _BV(COM0B1);
-    TCCR0A |= _BV(WGM00) | _BV(WGM01) | _BV(WGM02);
-    TCCR0B |= _BV(CS00);
+	note_index = -1;
+	delay = 1;
 
-    // Timer 1 for amplitude (waveform and envelope) updates
-    // CTC, 16 MHz
-    // 18 KHz interrupt (16 Mhz / 889)
-    TCCR1B |= _BV(WGM12);
-    TCCR1B |= _BV(CS10);
-    TIMSK1 |= _BV(OCIE1A);
-    OCR1A = 667;
+	// Timer 0 for PWM of speaker pins (to simulate amplitudes)
+	// Fast PWM, 16 MHz tick
+	TCCR0A |= _BV(COM0A1) | _BV(COM0B1);
+	TCCR0A |= _BV(WGM00) | _BV(WGM01) | _BV(WGM02);
+	TCCR0B |= _BV(CS00);
 
-    // turn on interrupts
-    sei();
+	// Timer 1 for amplitude (waveform and envelope) updates
+	// CTC, 16 MHz
+	// 18 KHz interrupt (16 Mhz / 889)
+	TCCR1B |= _BV(WGM12);
+	TCCR1B |= _BV(CS10);
+	TIMSK1 |= _BV(OCIE1A);
+	OCR1A = 667;
 
-    while (true);
+	// Turn on interrupts
+	sei();
 
-    return 0;
+	while (true);
+
+	return 0;
 }
 
 ISR(TIMER1_COMPA_vect) {
-    uint16_t out = 0;
-    for (int i = 0; i < NUM_CHANNELS; i++) {
-        channels[i].count += channels[i].add;
-        uint8_t env_index = channels[i].env_count >> 8;
+	uint16_t out = 0;
+	for (int i = 0; i < NUM_CHANNELS; i++) {
+		channels[i].count += channels[i].add;
+		uint8_t env_index = channels[i].env_count >> 8;
 
-        if (env_index < 128) {
-            channels[i].env_count++;        
-            uint16_t wave_val = waveform[channels[i].count >> 10];
-            uint8_t env_val = envelope[env_index];
-            out += ((wave_val * env_val) >> 8);
-        }
+		if (env_index < 128) {
+			channels[i].env_count++;        
+			uint16_t wave_val = waveform[channels[i].count >> 10];
+			uint8_t env_val = envelope[env_index];
+			out += ((wave_val * env_val) >> 8);
+		}
 
-        if (i == NUM_CHANNELS / 2 - 1) {
-            OCR0A = out >> 1;
-            out = 0;
-        }
-    }
-    OCR0B = out >> 1;
+		if (i == NUM_CHANNELS / 2 - 1) {
+			OCR0A = out >> 1;
+			out = 0;
+		}
+	}
+	OCR0B = out >> 1;
 
-    // note updating and key handler
-    update_count++;
-    if (update_count == 92) {  // 138 at 36khz
-        update_count = 0;
+	// Note updating and key handler
+	update_count++;
+	if (update_count == 92) {  // 138 at 36khz
+		update_count = 0;
 
-        if (autoMode) {
-            delay--;
-            if (delay == 0) {
-                note_index++;
-                uint8_t note = pgm_read_byte(&(*songs[song_index])[note_index][0]);
-                uint16_t data = pgm_read_word(&(*songs[song_index])[note_index][1]);
-                uint8_t c_index = data & 0x3;
-                delay = data >> 3;
-                if (note != 0) {
-                    channels[c_index].add = notes_add[note + 12 - 21];
-                    channels[c_index].env_count = 0;
+		if (autoMode) {
+			delay--;
+			if (delay == 0) {
+				note_index++;
+				uint8_t note = pgm_read_byte(&(*songs[song_index])[note_index][0]);
+				uint16_t data = pgm_read_word(&(*songs[song_index])[note_index][1]);
+				uint8_t c_index = data & 0x3;
+				delay = data >> 3;
+				if (note != 0) {
+					channels[c_index].add = notes_add[note + 12 - 21];
+					channels[c_index].env_count = 0;
 
-                    led_delay = delay / 3;
+					led_delay = delay / 3;
 
-                } else {
-                    if (delay == 0) {
-                        // end of song
-                        delay = 500;
-                        note_index = 0;//-1;
-                        //song_index++;
-                        //if (song_index == NUM_SONGS) song_index = 0;
-                    }
-                }
-                channels[c_index].count = 0;            
-            }
-        }
+				} else {
+					if (delay == 0) {
+						// --- End of song ---
+						delay = 500;
+						note_index = 0;//-1;
+						//song_index++;
+						//if (song_index == NUM_SONGS) song_index = 0;
+					}
+				}
+				channels[c_index].count = 0;            
+			}
+		}
 
-    }
+	}
 
 }
 
